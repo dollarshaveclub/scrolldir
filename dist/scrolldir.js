6746460bc2801ce3cf5a4c4714046cb304680aa8
Patrick Fisher <patrick@pwfisher.com>
2017-03-21 21:32:42 -0700

    fix removeEventListener - move handler outside function scope so reference is to same object and it can be removed

--- dist/scrolldir.js
+++ dist/scrolldir.js
@@ -4,77 +4,82 @@
 	(global.scrollDir = factory());
 }(this, (function () { 'use strict';
 
+var defaults = {
+  el: document.documentElement,
+  win: window,
+  attribute: 'data-scrolldir'
+};
+var el = void 0;
+var win = void 0;
+var attribute = void 0;
+var body = document.body;
+var historyLength = 32; // Ticks to keep in history.
+var historyMaxAge = 512; // History data time-to-live (ms).
+var thresholdPixels = 64; // Ignore moves smaller than this.
+var history = Array(historyLength);
+var dir = 'down'; // 'up' or 'down'
+var e = void 0; // last scroll event
+var pivot = void 0; // "high-water mark"
+var pivotTime = 0;
+
+function tick() {
+  var y = win.scrollY;
+  var t = e.timeStamp;
+  var furthest = dir === 'down' ? Math.max : Math.min;
+
+  // Apply bounds to handle rubber banding
+  var yMax = body.offsetHeight - win.innerHeight;
+  y = Math.max(0, y);
+  y = Math.min(yMax, y);
+
+  // Update history
+  history.unshift({ y: y, t: t });
+  history.pop();
+
+  // Are we continuing in the same direction?
+  if (y === furthest(pivot, y)) {
+    // Update "high-water mark" for current direction
+    pivotTime = t;
+    pivot = y;
+    return;
+  }
+  // else we have backed off high-water mark
+
+  // Apply max age to find current reference point
+  var cutoffTime = t - historyMaxAge;
+  if (cutoffTime > pivotTime) {
+    pivot = y;
+    for (var i = 0; i < historyLength; i += 1) {
+      if (!history[i] || history[i].t < cutoffTime) break;
+      pivot = furthest(pivot, history[i].y);
+    }
+  }
+
+  // Have we exceeded threshold?
+  if (Math.abs(y - pivot) > thresholdPixels) {
+    pivot = y;
+    pivotTime = t;
+    dir = dir === 'down' ? 'up' : 'down';
+    el.setAttribute(attribute, dir);
+  }
+}
+
+function handler(event) {
+  e = event;
+  return win.requestAnimationFrame(tick);
+}
+
 function scrollDir(opts) {
-  var defaults = {
-    el: document.documentElement,
-    win: window,
-    attribute: 'data-scrolldir'
-  };
-  var el = opts && opts.el || defaults.el;
-  var win = opts && opts.win || defaults.win;
-  var attribute = opts && opts.attribute || defaults.attribute;
-  var body = document.body;
-  var historyLength = 32; // Ticks to keep in history.
-  var historyMaxAge = 512; // History data time-to-live (ms).
-  var thresholdPixels = 64; // Ignore moves smaller than this.
-  var history = Array(historyLength);
-  var dir = 'down'; // 'up' or 'down'
-  var e = void 0; // last scroll event
-  var pivot = void 0; // "high-water mark"
-  var pivotTime = 0;
-  var tick = function tickFunc() {
-    var y = win.scrollY;
-    var t = e.timeStamp;
-    var furthest = dir === 'down' ? Math.max : Math.min;
-
-    // Apply bounds to handle rubber banding
-    var yMax = body.offsetHeight - win.innerHeight;
-    y = Math.max(0, y);
-    y = Math.min(yMax, y);
-
-    // Update history
-    history.unshift({ y: y, t: t });
-    history.pop();
-
-    // Are we continuing in the same direction?
-    if (y === furthest(pivot, y)) {
-      // Update "high-water mark" for current direction
-      pivotTime = t;
-      pivot = y;
-      return;
-    }
-    // else we have backed off high-water mark
-
-    // Apply max age to find current reference point
-    var cutoffTime = t - historyMaxAge;
-    if (cutoffTime > pivotTime) {
-      pivot = y;
-      for (var i = 0; i < historyLength; i += 1) {
-        if (!history[i] || history[i].t < cutoffTime) break;
-        pivot = furthest(pivot, history[i].y);
-      }
-    }
-
-    // Have we exceeded threshold?
-    if (Math.abs(y - pivot) > thresholdPixels) {
-      pivot = y;
-      pivotTime = t;
-      dir = dir === 'down' ? 'up' : 'down';
-      el.setAttribute(attribute, dir);
-    }
-  };
-
-  var handler = function handlerFunc(event) {
-    e = event;
-    return win.requestAnimationFrame(tick);
-  };
+  el = opts && opts.el || defaults.el;
+  win = opts && opts.win || defaults.win;
+  attribute = opts && opts.attribute || defaults.attribute;
 
   // If opts.off, turn it off
   // - set html[data-scrolldir="off"]
   // - remove the event listener
   if (opts && opts.off === true) {
-    win.removeEventListener('scroll', handler);
-    return el.setAttribute(attribute, 'off');
+    el.setAttribute(attribute, 'off');
+    return win.removeEventListener('scroll', handler);
   }
 
   // else, turn it on
--- src/scrolldir.js
+++ src/scrolldir.js
@@ -1,74 +1,79 @@
+const defaults = {
+  el: document.documentElement,
+  win: window,
+  attribute: 'data-scrolldir',
+};
+let el;
+let win;
+let attribute;
+const body = document.body;
+const historyLength = 32; // Ticks to keep in history.
+const historyMaxAge = 512; // History data time-to-live (ms).
+const thresholdPixels = 64; // Ignore moves smaller than this.
+const history = Array(historyLength);
+let dir = 'down'; // 'up' or 'down'
+let e; // last scroll event
+let pivot; // "high-water mark"
+let pivotTime = 0;
+
+function tick() {
+  let y = win.scrollY;
+  const t = e.timeStamp;
+  const furthest = dir === 'down' ? Math.max : Math.min;
+
+  // Apply bounds to handle rubber banding
+  const yMax = body.offsetHeight - win.innerHeight;
+  y = Math.max(0, y);
+  y = Math.min(yMax, y);
+
+  // Update history
+  history.unshift({ y, t });
+  history.pop();
+
+  // Are we continuing in the same direction?
+  if (y === furthest(pivot, y)) {
+    // Update "high-water mark" for current direction
+    pivotTime = t;
+    pivot = y;
+    return;
+  }
+  // else we have backed off high-water mark
+
+  // Apply max age to find current reference point
+  const cutoffTime = t - historyMaxAge;
+  if (cutoffTime > pivotTime) {
+    pivot = y;
+    for (let i = 0; i < historyLength; i += 1) {
+      if (!history[i] || history[i].t < cutoffTime) break;
+      pivot = furthest(pivot, history[i].y);
+    }
+  }
+
+  // Have we exceeded threshold?
+  if (Math.abs(y - pivot) > thresholdPixels) {
+    pivot = y;
+    pivotTime = t;
+    dir = dir === 'down' ? 'up' : 'down';
+    el.setAttribute(attribute, dir);
+  }
+}
+
+function handler(event) {
+  e = event;
+  return win.requestAnimationFrame(tick);
+}
+
 export default function scrollDir(opts) {
-  const defaults = {
-    el: document.documentElement,
-    win: window,
-    attribute: 'data-scrolldir',
-  };
-  const el = (opts && opts.el) || defaults.el;
-  const win = (opts && opts.win) || defaults.win;
-  const attribute = (opts && opts.attribute) || defaults.attribute;
-  const body = document.body;
-  const historyLength = 32; // Ticks to keep in history.
-  const historyMaxAge = 512; // History data time-to-live (ms).
-  const thresholdPixels = 64; // Ignore moves smaller than this.
-  const history = Array(historyLength);
-  let dir = 'down'; // 'up' or 'down'
-  let e; // last scroll event
-  let pivot; // "high-water mark"
-  let pivotTime = 0;
-  const tick = function tickFunc() {
-    let y = win.scrollY;
-    const t = e.timeStamp;
-    const furthest = dir === 'down' ? Math.max : Math.min;
-
-    // Apply bounds to handle rubber banding
-    const yMax = body.offsetHeight - win.innerHeight;
-    y = Math.max(0, y);
-    y = Math.min(yMax, y);
-
-    // Update history
-    history.unshift({ y, t });
-    history.pop();
-
-    // Are we continuing in the same direction?
-    if (y === furthest(pivot, y)) {
-      // Update "high-water mark" for current direction
-      pivotTime = t;
-      pivot = y;
-      return;
-    }
-    // else we have backed off high-water mark
-
-    // Apply max age to find current reference point
-    const cutoffTime = t - historyMaxAge;
-    if (cutoffTime > pivotTime) {
-      pivot = y;
-      for (let i = 0; i < historyLength; i += 1) {
-        if (!history[i] || history[i].t < cutoffTime) break;
-        pivot = furthest(pivot, history[i].y);
-      }
-    }
-
-    // Have we exceeded threshold?
-    if (Math.abs(y - pivot) > thresholdPixels) {
-      pivot = y;
-      pivotTime = t;
-      dir = dir === 'down' ? 'up' : 'down';
-      el.setAttribute(attribute, dir);
-    }
-  };
-
-  const handler = function handlerFunc(event) {
-    e = event;
-    return win.requestAnimationFrame(tick);
-  };
+  el = (opts && opts.el) || defaults.el;
+  win = (opts && opts.win) || defaults.win;
+  attribute = (opts && opts.attribute) || defaults.attribute;
 
   // If opts.off, turn it off
   // - set html[data-scrolldir="off"]
   // - remove the event listener
   if (opts && opts.off === true) {
-    win.removeEventListener('scroll', handler);
-    return el.setAttribute(attribute, 'off');
+    el.setAttribute(attribute, 'off');
+    return win.removeEventListener('scroll', handler);
   }
 
   // else, turn it on
