5981809339808b481a6ec7f4278d2178d827e920
Jeff Wainwright <jjwainwright2@gmail.com>
2017-09-15 14:51:26 -0700

    [update] updates linting (#37)

--- src/scrolldir.js
+++ src/scrolldir.js
@@ -3,85 +3,85 @@ const defaults = {
   win: window,
   attribute: 'data-scrolldir',
   dir: 'down',
-};
-let el;
-let win;
-let attribute;
-let dir; // 'up' or 'down'
-const body = document.body;
-const historyLength = 32; // Ticks to keep in history.
-const historyMaxAge = 512; // History data time-to-live (ms).
-const thresholdPixels = 64; // Ignore moves smaller than this.
-const history = Array(historyLength);
-let e; // last scroll event
-let pivot; // "high-water mark"
-let pivotTime = 0;
+}
+let el
+let win
+let attribute
+let dir // 'up' or 'down'
+const body = document.body
+const historyLength = 32 // Ticks to keep in history.
+const historyMaxAge = 512 // History data time-to-live (ms).
+const thresholdPixels = 64 // Ignore moves smaller than this.
+const history = Array(historyLength)
+let e // last scroll event
+let pivot // "high-water mark"
+let pivotTime = 0
 
 function tick() {
-  let y = win.scrollY || win.pageYOffset;
-  const t = e.timeStamp;
-  const furthest = dir === 'down' ? Math.max : Math.min;
+  let y = win.scrollY || win.pageYOffset
+  const t = e.timeStamp
+  const furthest = dir === 'down' ? Math.max : Math.min
 
   // Apply bounds to handle rubber banding
-  const yMax = body.offsetHeight - win.innerHeight;
-  y = Math.max(0, y);
-  y = Math.min(yMax, y);
+  const yMax = body.offsetHeight - win.innerHeight
+  y = Math.max(0, y)
+  y = Math.min(yMax, y)
 
   // Update history
-  history.unshift({ y, t });
-  history.pop();
+  history.unshift({ y, t })
+  history.pop()
 
   // Are we continuing in the same direction?
   if (y === furthest(pivot, y)) {
     // Update "high-water mark" for current direction
-    pivotTime = t;
-    pivot = y;
-    return;
+    pivotTime = t
+    pivot = y
+    return
   }
   // else we have backed off high-water mark
 
   // Apply max age to find current reference point
-  const cutoffTime = t - historyMaxAge;
+  const cutoffTime = t - historyMaxAge
   if (cutoffTime > pivotTime) {
-    pivot = y;
+    pivot = y
     for (let i = 0; i < historyLength; i += 1) {
-      if (!history[i] || history[i].t < cutoffTime) break;
-      pivot = furthest(pivot, history[i].y);
+      if (!history[i] || history[i].t < cutoffTime) break
+      pivot = furthest(pivot, history[i].y)
     }
   }
 
   // Have we exceeded threshold?
   if (Math.abs(y - pivot) > thresholdPixels) {
-    pivot = y;
-    pivotTime = t;
-    dir = dir === 'down' ? 'up' : 'down';
-    el.setAttribute(attribute, dir);
+    pivot = y
+    pivotTime = t
+    dir = dir === 'down' ? 'up' : 'down'
+    el.setAttribute(attribute, dir)
   }
 }
 
 function handler(event) {
-  e = event;
-  return win.requestAnimationFrame(tick);
+  e = event
+  return win.requestAnimationFrame(tick)
 }
 
 export default function scrollDir(opts) {
-  el = (opts && opts.el) || defaults.el;
-  win = (opts && opts.win) || defaults.win;
-  attribute = (opts && opts.attribute) || defaults.attribute;
-  dir = (opts && opts.direction) || defaults.dir;
+  el = (opts && opts.el) || defaults.el
+  win = (opts && opts.win) || defaults.win
+  attribute = (opts && opts.attribute) || defaults.attribute
+  dir = (opts && opts.direction) || defaults.dir
 
   // If opts.off, turn it off
   // - set html[data-scrolldir="off"]
   // - remove the event listener
   if (opts && opts.off === true) {
-    el.setAttribute(attribute, 'off');
-    return win.removeEventListener('scroll', handler);
+    el.setAttribute(attribute, 'off')
+    return win.removeEventListener('scroll', handler)
   }
 
   // else, turn it on
   // - set html[data-scrolldir="down"]
   // - add the event listener
-  pivot = win.scrollY || win.pageYOffset;
-  el.setAttribute(attribute, dir);
-  return win.addEventListener('scroll', handler);
+  pivot = win.scrollY || win.pageYOffset
+  el.setAttribute(attribute, dir)
+  return win.addEventListener('scroll', handler)
 }
--- tests/standard/test.js
+++ tests/standard/test.js
@@ -1,20 +1,20 @@
-QUnit.test( "hello test", function(assert) {
-  scrollDir();
-  assert.ok(1 == "1", "Passed!");
-});
-QUnit.test( "Is scroll intent there?", function(assert) {
-  scrollDir();
-  assert.equal(document.querySelector('[data-scrolldir]').getAttribute('data-scrolldir'), 'down', 'there should be a scroll intent');
-});
-QUnit.test( "scrollDir off", function(assert) {
-  scrollDir({off: true});
-  assert.equal(document.querySelector('[data-scrolldir]').getAttribute('data-scrolldir'), 'off', 'scrolldir should read as off');
-});
-QUnit.test( "scrollDir has a different attribute", function(assert) {
-  scrollDir({attribute: 'data-scroll-direction'});
-  assert.equal(document.querySelector('[data-scroll-direction]').getAttribute('data-scroll-direction'), 'down', 'scrolldir attribute should be scroll-direction');
-});
-QUnit.test( "scrollDir has a different attribute", function(assert) {
-  scrollDir({el: document.body, attribute: 'data-scroll-new-attribute'});
-  assert.equal(document.querySelector('[data-scroll-new-attribute]').getAttribute('data-scroll-new-attribute'), 'down', 'scrolldir attribute should be on a new element');
-});
+QUnit.test('hello test', (assert) => {
+  scrollDir()
+  assert.ok(1 == '1', 'Passed!')
+})
+QUnit.test('Is scroll intent there?', (assert) => {
+  scrollDir()
+  assert.equal(document.querySelector('[data-scrolldir]').getAttribute('data-scrolldir'), 'down', 'there should be a scroll intent')
+})
+QUnit.test('scrollDir off', (assert) => {
+  scrollDir({ off: true })
+  assert.equal(document.querySelector('[data-scrolldir]').getAttribute('data-scrolldir'), 'off', 'scrolldir should read as off')
+})
+QUnit.test('scrollDir has a different attribute', (assert) => {
+  scrollDir({ attribute: 'data-scroll-direction' })
+  assert.equal(document.querySelector('[data-scroll-direction]').getAttribute('data-scroll-direction'), 'down', 'scrolldir attribute should be scroll-direction')
+})
+QUnit.test('scrollDir has a different attribute', (assert) => {
+  scrollDir({ el: document.body, attribute: 'data-scroll-new-attribute' })
+  assert.equal(document.querySelector('[data-scroll-new-attribute]').getAttribute('data-scroll-new-attribute'), 'down', 'scrolldir attribute should be on a new element')
+})
