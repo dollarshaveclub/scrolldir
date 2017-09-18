8cc76d51c84a83a19eb6f2a548971f2ea7d0e3a2
Jeff Wainwright <jjwainwright2@gmail.com>
2017-09-18 15:02:20 -0700

    Revert 37 lint update (#39)
    * Revert "chore(package): update rollup to version 0.50.0 (#38)"
    
    This reverts commit 7993725c175e604a29bfd17660ef1567cdd900b2.
    
    * Revert "[update] updates linting (#37)"
    
    This reverts commit 5981809339808b481a6ec7f4278d2178d827e920.
    
    * [bug] fixes bad compilation

--- tests/off/test.js
+++ tests/off/test.js
@@ -1,5 +1,5 @@
-QUnit.test('Is scrolldir there?', (assert) => {
-  scrollDir()
-  scrollDir({ off: true })
-  assert.equal(document.querySelector('[data-scrolldir]').getAttribute('data-scrolldir'), 'off', 'there should not be scrolldir')
-})
+QUnit.test( "Is scrolldir there?", function(assert) {
+  scrollDir();
+  scrollDir({off: true});
+  assert.equal(document.querySelector('[data-scrolldir]').getAttribute('data-scrolldir'), 'off', 'there should not be scrolldir');
+});
