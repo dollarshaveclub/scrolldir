a9f98c6224c0eb668ad793b13ee50e70b97925c1
Jeff Wainwright <yowainwright@gmail.com>
2018-09-10 00:38:21 -0700

    [init] adds module file and module support (#80)
    * [init] adds module file and module support
    
    * [update] optimizes build
    
    * [update] updates travis config for CI

--- dist/scrolldir.js
+++ dist/scrolldir.js
@@ -1,92 +1,107 @@
+/**
+  scrolldir - Vertical scroll direction in CSS
+  @version v1.4.0
+  @link https://github.com/dollarshaveclub/scrolldir.git
+  @author Patrick Fisher <patrick@pwfisher.com>
+  @license MIT
+**/
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global.scrollDir = factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.scrollDir = factory());
 }(this, (function () { 'use strict';
 
-var attribute = 'data-scrolldir';
-var dir = 'down'; // 'up' or 'down'
-var el = document.documentElement;
-var win = window;
-var body = document.body;
-var historyLength = 32; // Ticks to keep in history.
-var historyMaxAge = 512; // History data time-to-live (ms).
-var thresholdPixels = 64; // Ignore moves smaller than this.
-var history = Array(historyLength);
-var e = void 0; // last scroll event
-var pivot = void 0; // "high-water mark"
-var pivotTime = 0;
+  var attribute = 'data-scrolldir';
+  var dir = 'down'; // 'up' or 'down'
 
-function tick() {
-  var y = win.scrollY || win.pageYOffset;
-  var t = e.timeStamp;
-  var furthest = dir === 'down' ? Math.max : Math.min;
+  var el = document.documentElement;
+  var win = window;
+  var body = document.body;
+  var historyLength = 32; // Ticks to keep in history.
 
-  // Apply bounds to handle rubber banding
-  var yMax = body.offsetHeight - win.innerHeight;
-  y = Math.max(0, y);
-  y = Math.min(yMax, y);
+  var historyMaxAge = 512; // History data time-to-live (ms).
 
-  // Update history
-  history.unshift({ y: y, t: t });
-  history.pop();
+  var thresholdPixels = 64; // Ignore moves smaller than this.
 
-  // Are we continuing in the same direction?
-  if (y === furthest(pivot, y)) {
-    // Update "high-water mark" for current direction
-    pivotTime = t;
-    pivot = y;
-    return;
-  }
-  // else we have backed off high-water mark
+  var history = Array(historyLength);
+  var e; // last scroll event
 
-  // Apply max age to find current reference point
-  var cutoffTime = t - historyMaxAge;
-  if (cutoffTime > pivotTime) {
-    pivot = y;
-    for (var i = 0; i < historyLength; i += 1) {
-      if (!history[i] || history[i].t < cutoffTime) break;
-      pivot = furthest(pivot, history[i].y);
+  var pivot; // "high-water mark"
+
+  var pivotTime = 0;
+
+  function tick() {
+    var y = win.scrollY || win.pageYOffset;
+    var t = e.timeStamp;
+    var furthest = dir === 'down' ? Math.max : Math.min; // Apply bounds to handle rubber banding
+
+    var yMax = body.scrollHeight - win.innerHeight;
+    y = Math.max(0, y);
+    y = Math.min(yMax, y); // Update history
+
+    history.unshift({
+      y: y,
+      t: t
+    });
+    history.pop(); // Are we continuing in the same direction?
+
+    if (y === furthest(pivot, y)) {
+      // Update "high-water mark" for current direction
+      pivotTime = t;
+      pivot = y;
+      return;
+    } // else we have backed off high-water mark
+    // Apply max age to find current reference point
+
+
+    var cutoffTime = t - historyMaxAge;
+
+    if (cutoffTime > pivotTime) {
+      pivot = y;
+
+      for (var i = 0; i < historyLength; i += 1) {
+        if (!history[i] || history[i].t < cutoffTime) break;
+        pivot = furthest(pivot, history[i].y);
+      }
+    } // Have we exceeded threshold?
+
+
+    if (Math.abs(y - pivot) > thresholdPixels) {
+      pivot = y;
+      pivotTime = t;
+      dir = dir === 'down' ? 'up' : 'down';
+      el.setAttribute(attribute, dir);
     }
   }
 
-  // Have we exceeded threshold?
-  if (Math.abs(y - pivot) > thresholdPixels) {
-    pivot = y;
-    pivotTime = t;
-    dir = dir === 'down' ? 'up' : 'down';
+  function handler(event) {
+    e = event;
+    return win.requestAnimationFrame(tick);
+  }
+
+  function scrollDir(opts) {
+    if (opts) {
+      if (opts.attribute) attribute = opts.attribute;
+      if (opts.el) el = opts.el;
+      if (opts.win) win = opts.win;
+      if (opts.dir) dir = opts.dir; // If opts.off, turn it off
+      // - set html[data-scrolldir="off"]
+      // - remove the event listener
+
+      if (opts.off === true) {
+        el.setAttribute(attribute, 'off');
+        return win.removeEventListener('scroll', handler);
+      }
+    } // else, turn it on
+    // - set html[data-scrolldir="down"]
+    // - add the event listener
+
+
+    pivot = win.scrollY || win.pageYOffset;
     el.setAttribute(attribute, dir);
-  }
-}
-
-function handler(event) {
-  e = event;
-  return win.requestAnimationFrame(tick);
-}
-
-function scrollDir(opts) {
-  if (opts) {
-    if (opts.attribute) attribute = opts.attribute;
-    if (opts.el) el = opts.el;
-    if (opts.win) win = opts.win;
-    if (opts.dir) dir = opts.dir;
-    // If opts.off, turn it off
-    // - set html[data-scrolldir="off"]
-    // - remove the event listener
-    if (opts.off === true) {
-      el.setAttribute(attribute, 'off');
-      return win.removeEventListener('scroll', handler);
-    }
+    return win.addEventListener('scroll', handler);
   }
 
-  // else, turn it on
-  // - set html[data-scrolldir="down"]
-  // - add the event listener
-  pivot = win.scrollY || win.pageYOffset;
-  el.setAttribute(attribute, dir);
-  return win.addEventListener('scroll', handler);
-}
-
-return scrollDir;
+  return scrollDir;
 
 })));
--- rollup.config.js
+++ rollup.config.js
@@ -3,30 +3,105 @@
 // rollup -c --environment entry:.auto => builds self envoking scrolldir
 
 import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+import { uglify } from 'rollup-plugin-uglify'
 
-const name = process.env.entry ? '.auto' : ''
+import {
+  author,
+  description,
+  homepage,
+  license,
+  main,
+  module,
+  name,
+  version,
+} from './package.json'
 
-export default {
-  input: `src/scrolldir${name}.js`,
-  plugins: [
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-    }),
-    commonjs(),
-    babel({
-      babelrc: false,
-      presets: [['@babel/preset-env', { modules: false}]],
-      exclude: 'node_modules/**',
-    }),
-  ],
+const loose = true
+
+const babelSetup = {
+  babelrc: false,
+  presets: [['@babel/preset-env', { modules: false, loose }]],
+  exclude: 'node_modules/**',
+}
+
+const uglifyOutput = {
   output: {
-    dest: `dist/scrolldir${name}.js`,
-    format: 'umd',
-    name: `scrollDir${name}`,
-    sourceMap: false, // removes the souremap at the bottom of the file
+    comments: (node, comment) => {
+      const text = comment.value
+      const type = comment.type
+      if (type === 'comment2') {
+        // multiline comment
+        return /@preserve|@license|@cc_on/i.test(text)
+      }
+    },
+  },
+}
+
+const banner = `/**
+  ${name} - ${description}
+  @version v${version}
+  @link ${homepage}
+  @author ${author}
+  @license ${license}
+**/`
+
+const ensureArray = maybeArr => Array.isArray(maybeArr) ? maybeArr : [maybeArr]
+
+const createConfig = ({ input, output, env } = {}) => {
+  const plugins = [
+    babel(babelSetup),
+    replace({ 'VERSION': JSON.stringify(version).replace(/"/g, '') }),
+  ]
+
+  if (env === 'production') plugins.push(uglify(uglifyOutput))
+
+  return {
+    input,
+    plugins,
+    output: ensureArray(output).map(format =>
+      Object.assign(
+        {},
+        format,
+        {
+          banner,
+          name: 'scrollDir',
+        }
+      )
+    ),
   }
 }
+
+export default [
+  createConfig({
+    input: 'src/scrolldir.js',
+    output: [
+      { file: main, format: 'umd' },
+      { file: module, format: 'es' },
+    ],
+  }),
+  createConfig({
+    input: 'src/scrolldir.js',
+    output: {
+      file: 'dist/scrolldir.min.js',
+      format: 'umd',
+    },
+    env: 'production',
+  }),
+  createConfig({
+    input: 'src/scrolldir.auto.js',
+    output: {
+      file: 'dist/scrolldir.auto.js',
+      format: 'umd',
+    },
+  }),
+  createConfig({
+    input: 'src/scrolldir.auto.js',
+    output: {
+      file: 'dist/scrolldir.auto.min.js',
+      format: 'umd',
+    },
+    env: 'production',
+  }),
+]
+
